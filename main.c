f193f074a5d6a49a875a6d2316fc9bbbe89c7707
TddKim0712 <rladbcks0712@naver.com>
2026-02-07 21:37:24 +0900

    added select non-blocking, based on FD_SET

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <linux/input.h>
-#include <fcntl.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
-#include <ioctl.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
 
 
 // Find which /dev/input/eventX corresponds to a keyboard
@@ -57,37 +58,118 @@ char* key_event_location() {
 
 int main(int argc, char* argv[]){
 
-
-    if (argc != 2){
+    // enable this if you want to read linux/input/eventX manually
+    // if (argc != 2){
         
-        printf("usage: %s <event-file-path>", argv[0]);
-        exit(-1);
+    //     printf("usage: %s <event-file-path>", argv[0]);
+    //     exit(-1);
 
+    // }
+    char* event_file_path = key_event_location();
+    if (!event_file_path){
+        printf("key event not found\n");
+        exit(-1);   
     }
+
     printf("keylogger enabled\n");
 
+// enable this if you read eventX manually
     // file descriptor : open event file -> read (get buffer)
-    int fd = open(argv[1], O_RDONLY, 0);    // argv[1] is where the event file is, only for reading
+  //  int fd = open(argv[1], O_RDONLY, 0);    // argv[1] is where the event file is, only for reading
     
-    printf("opened  file descriptor: %d\n", fd);
+         // file descriptor : open event file and get buffer
+    int fd = open(event_file_path, O_RDONLY, 0);   
+        // original flags of fd after open event file
+    int flags = fcntl(fd, F_GETFL, 0);            
+         //  fd includes two types of info
+         //  info determined when OPEN
+         // info  about STATEs
+         // O_NONBLOCK is about STATEs, so it can be modified after opened
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+        //using only fcntl(fd, F_SETFL, O_NONBLOCK) will overwrite fd flag which was created when opened  
+        // set the state -> (flags | O_NONBLOCK) -> "OR" means adding attributes
 
-    struct input_event ev;
+
+            ///// actually identical to """open(path, O_RDONLY | O_NONBLOCK)"""";
+            /// used fcntl, easier to fix partially 
+
+    printf("opened file descriptor: %d\n", fd);
+
+   
+
+    for(;;){
+        
+    // these variables (fd_set, tv )  are modified after select()  
+    // thus need to re-init when the loop starts
+    fd_set readfds;     // set of fd, what i really want to keep looking at
+    struct timeval tv;      // timeout to await my key input
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+
+    FD_ZERO(&readfds);
+      // init fd set all zero
+    FD_SET(STDIN_FILENO, &readfds);    
+      // STDIN -> target whether terminal input comes
+    FD_SET(fd, &readfds);               
+      // fd -> key event , target whether event triggered
+
+    int maxnum = STDIN_FILENO > fd ? STDIN_FILENO +1 : fd + 1 ;
+    int ret = select(maxnum, &readfds, NULL, NULL, &tv);
+
+    // check if select returned false value
+    if (ret < 0){
+        printf("select returned error\n");
+        break;
+    }
+    if (ret == 0){
+        printf("select timeout\n");
+        continue;
+    }
+
+    if (FD_ISSET(fd, &readfds)){
+        struct input_event ev;
     // input_event is already declared in linux/input
     // contains RAW data of event:  time, type, code, value
     // in keylogger, keyboard input should be checked
 
+       ssize_t n = read(fd, &ev, sizeof(ev));
+       // n is returned value of read
+       
+        if (n > 0 && ev.type == EV_KEY) {
+		printf("%ld.%06ld\n-----  %d %d %d\n"
+				,ev.time.tv_sec, ev.time.tv_usec, ev.type, ev.code, ev.value);
+	    }
+    }
+
+    if (FD_ISSET(STDIN_FILENO, &readfds)){
+        if (getchar() == 'q') {
+                break;
+        }
+    }
+}
+    close(fd);
+}
+    
+    
+
+/*
+
+Keylogger prototype:
+- using read() system call
+- blocking -> the terminal cannot do other things else
     while(1){
         int n = read(fd, &ev, sizeof(ev));  // get buffer of input_event from fd(opened event)
         if (n < 0 || sizeof(ev)< 1) {
             printf("event read failed\n");
             exit(-2);
         }
-       /*	
-        if (ev.type != EV_KEY){
-            printf("not keyboard event\n");
-            continue;
-        }
-	*/
+       
+        // if (ev.type != EV_KEY){
+        //     printf("not keyboard event\n");
+        //     continue;
+        // }
+	
 	// keyboard push -> not only EV_KEY but also others
         // if you print ev types to watch like below:
 	//	printf("%d %d\n",ev.type, ev.code);
@@ -101,9 +183,12 @@ int main(int argc, char* argv[]){
 				,ev.time.tv_sec, ev.time.tv_usec, ev.type, ev.code, ev.value);
 	}
 
-
-    }
-
+*/
 
 
-}
+
+    
+
+
+
+
